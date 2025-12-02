5ebf73675ca0420fa7157f0e5f39be93b7c871e1
Nordestino <batatas.pizzas@gmail.com>
2025-12-02 15:25:27 -0300

    base das entidades

--- CAMAAR_RAILS/app/models/form.rb
+++ CAMAAR_RAILS/app/models/form.rb
@@ -1,2 +1,10 @@
 class Form < ApplicationRecord
+
+  belongs_to :administrador
+  belongs_to :template
+  belongs_to :turma
+  
+  has_many :perguntas, dependent: :destroy
+  
+ has_and_belongs_to_many :alunos_respostas, class_name: 'Aluno', join_table: :alunos_formularios
 end
--- CAMAAR_RAILS/app/models/student.rb
+++ CAMAAR_RAILS/app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+  validates :matricula, presence: true, uniqueness: true
+  validates :usuario, presence: true, uniqueness: true
+
 end
