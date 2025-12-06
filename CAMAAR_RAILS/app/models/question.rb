21f6b7f4454b2f2b752f250a9b1d4a28d8414f2c
Nordestino <batatas.pizzas@gmail.com>
2025-12-06 00:38:20 -0300

    reiniciando em portugues

--- CAMAAR_RAILS/app/models/question.rb
+++
@@ -1,7 +0,0 @@
-class Question < ApplicationRecord
-  
-  belongs_to :template
-  belongs_to :formulario, optional: true
-  
-  validates :texto, presence: true
-end
--- CAMAAR_RAILS/app/models/template.rb
+++
@@ -1,8 +0,0 @@
-class Template < ApplicationRecord
-  
-  belongs_to :administrador
-  has_many :perguntas, dependent: :destroy
-  has_many :formularios, dependent: :destroy
-  
-  validates :nome, presence: true
-end
