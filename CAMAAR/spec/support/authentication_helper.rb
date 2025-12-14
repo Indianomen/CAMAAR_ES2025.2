5fd961813321fc81900aae6bbf4f6edb153c97ef
Redohairi <daviborges07@gmail.com>
2025-12-14 18:58:25 -0300

    test: Update authentication helper for Student namespace

--- CAMAAR/spec/support/authentication_helper.rb
+++ CAMAAR/spec/support/authentication_helper.rb
@@ -48,8 +48,30 @@ module AuthenticationHelper
   def login_as_aluno(aluno = nil)
     aluno ||= create(:aluno)
     set_session_for(aluno)
+    
+    # Mock current_user for all controllers
     allow(controller).to receive(:current_user).and_return(aluno)
-    allow(controller).to receive(:require_login).and_return(true)
+    allow(controller).to receive(:logged_in?).and_return(true)
+    
+    # Check controller type to decide what to mock
+    controller_name = controller.class.name
+    
+    case controller_name
+    when /^Student::/
+      # For Student namespace controllers
+      allow(controller).to receive(:current_aluno).and_return(aluno)
+      allow(controller).to receive(:authenticate_aluno!).and_return(true)
+      # Also ensure require_login passes
+      allow(controller).to receive(:require_login).and_return(true)
+    else
+      # For non-student controllers
+      allow(controller).to receive(:require_login).and_return(true)
+      # If they use current_aluno too, mock it
+      if controller.respond_to?(:current_aluno)
+        allow(controller).to receive(:current_aluno).and_return(aluno)
+      end
+    end
+    
     aluno
   end
   
